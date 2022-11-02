4efc7d9dc8db0fe826e38279c53789d59166cda5
adelaett <90894311+adelaett@users.noreply.github.com>
2022-11-02 15:50:18 +0100

    CI coq 8.13

--- flake.nix
+++ flake.nix
@@ -10,20 +10,30 @@
     in flake-utils.lib.eachSystem systems (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        coqPackages = pkgs.callPackage ./nix/packages.nix {};
+        self_pkgs = pkgs.callPackage ./nix/packages.nix {};
         python3 = pkgs.python3;
       in
       rec {
-        packages = {
+        packages = with self_pkgs; {
           catala = coqPackages.catala;
+          catala_8_5 = coqPackages_8_5.catala;
+          catala_8_6 = coqPackages_8_6.catala;
+          catala_8_7 = coqPackages_8_7.catala;
+          catala_8_8 = coqPackages_8_8.catala;
+          catala_8_9 = coqPackages_8_9.catala;
+          catala_8_10 = coqPackages_8_10.catala;
+          catala_8_11 = coqPackages_8_11.catala;
+          catala_8_12 = coqPackages_8_12.catala;
+          catala_8_13 = coqPackages_8_13.catala;
+          catala_8_14 = coqPackages_8_14.catala;
+          catala_8_15 = coqPackages_8_15.catala;
+          catala_8_16 = coqPackages_8_16.catala;
+          default = coqPackages.catala;
         };
-        defaultPackage = packages.catala;
         devShell = pkgs.mkShell {
           inputsFrom = [ packages.catala ];
           buildInputs = [
-            coqPackages.serapi
-            pkgs.ocamlPackages.dune_2
-            pkgs.ocaml
+            pkgs.coqPackages.serapi
             python3.pkgs.alectryon
           ];
         };
