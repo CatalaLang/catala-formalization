652be336ea89b0a711feceb0ac73124c3b581294
adelaett <90894311+adelaett@users.noreply.github.com>
2023-07-17 09:11:54 +0200

    removing Empty & Conflict

--- theories/syntax.v
+++ theories/syntax.v
@@ -14,16 +14,22 @@ Inductive term :=
     | Lam (t: {bind term})
     | Default (ts: list term) (tj tc: term)
         (* induction principle is not strong enought on the default. But this is known. *)
-    | Empty
-    | Conflict
 
     | Value (v: value)
     | Binop (op: op) (t1 t2: term)
 
 with value :=
     | Bool (b: bool)
-    | Int (i: Z.t)
+    | Int (i: Z)
     | Closure (t: term) (sigma: var -> value)
+.
+
+Definition get_op op i1 i2:=
+    match op, i1, i2 with
+    | Add, Int i1, Int i2 => Some (Int (Z.add i1 i2))
+    | Eq, Int i1, Int i2 => Some (Bool (Z.eqb i1 i2))
+    | _, _, _ => None
+    end
 .
 
 Definition value_eqb (v1 v2 : value) : bool :=
