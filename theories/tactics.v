43dbe5164d197a60c9f5a65d7994e4795454b5c6
adelaett <90894311+adelaett@users.noreply.github.com>
2023-07-17 09:11:43 +0200

    more tactics

--- theories/tactics.v
+++ theories/tactics.v
@@ -5,3 +5,16 @@ Tactic Notation "admit" "alain" := admit.
 Tactic Notation "admit" "alain" string(x):= admit.
 
 Tactic Notation "admit" := fail.
+
+Ltac inj :=
+  repeat match goal with
+    [h: _ = _ |- _] =>
+        try discriminate h;
+        try (injection h; intros; subst; clear h)
+    end.
+
+Ltac unpack :=
+    repeat match goal with
+    [h: _ /\ _ |- _ ] =>
+        destruct h
+    end.
