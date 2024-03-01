3e21511a08474b9a2b2b544f2494232ac390418d
adelaett <90894311+adelaett@users.noreply.github.com>
2024-03-01 17:30:07 +0100

    dependency graph between all lemmas.

---
+++ export/export.v
@@ -0,0 +1,28 @@
+Require dpdgraph.dpdgraph.
+
+From Catala Require
+  common
+  continuations
+  sequences
+  simulation_cred_to_sred
+  simulation_sred_to_cred
+  small_step
+  syntax
+  tactics
+  trans
+  typing
+.
+
+Set DependGraph File "catala.dpd".
+Print FileDependGraph
+  common
+  continuations
+  sequences
+  simulation_cred_to_sred
+  simulation_sred_to_cred
+  small_step
+  syntax
+  tactics
+  trans
+  typing
+.
