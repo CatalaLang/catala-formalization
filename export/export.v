eb40faa102292acbe1fe3e87d71954d7c19e6ef7
adelaett <90894311+adelaett@users.noreply.github.com>
2024-03-29 10:16:35 +0100

    export

--- export/export.v
+++ export/export.v
@@ -1,5 +1,8 @@
 Require dpdgraph.dpdgraph.
 
+
+(* This file exists only to export the fine-grain dependency graph between lemmas, definitions and theorems. It makes it possible for dead-lemmas elimination from a set of results in the paper. *)
+
 From Catala Require
   common
   continuations
