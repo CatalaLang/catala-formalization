3f54373b03405e9dc5b0278a6621def99ead5867
adelaett <90894311+adelaett@users.noreply.github.com>
2025-02-17 12:43:54 +0100

    almost there

--- docker.nix
+++ docker.nix
@@ -1,6 +1,12 @@
-{ dockerTools, coqPackages, coq, dune_3, name, busybox}:
+{ dockerTools, coqPackages, coq, dune_3, name, git, ocaml, gcc, busybox}:
 dockerTools.buildLayeredImage {
   name = name;
   tag = "latest";
-  contents = [ coq coqPackages.autosubst dune_3 busybox ];
+  contents = [ coq coqPackages.autosubst dune_3 git ocaml gcc busybox];
+  fakeRootCommands = ''
+    mkdir -p ./tmp
+    chmod 1777 ./tmp
+  '';
+
+
 }
